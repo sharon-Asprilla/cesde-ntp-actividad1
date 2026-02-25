d546721250304ebffdf82633ae4e772c87919388
sharon <sharonasprilla1724@gmail.com>
2026-02-25 13:02:48 -0500

    cambio 2

--- ejercicios/ejercicio_06.py
+++ ejercicios/ejercicio_06.py
@@ -2,17 +2,33 @@
 
 # Ejercicio 1: Crea una lista con los nombres de 3 colores e imprímela.
 # Escribe tu código debajo de esta línea:
+list = ["morado", " azul", "negro"]
+print(list)
+
 
 
 # Ejercicio 2: Crea una tupla con 2 coordenadas (ej. x, y) e imprímela.
 # (Nota: las tuplas no se pueden modificar una vez creadas)
 # Escribe tu código debajo de esta línea:
+tupla = ( "x", "y")
+print(tupla)
 
 
 # Ejercicio 3: Crea un conjunto (set) con 3 de tus frutas favoritas e imprímelo.
 # Escribe tu código debajo de esta línea:
+frutas = ["manzana", "banana", "cereza"]
+conjunto = set(frutas)
+print(conjunto)
+
+
 
 
 # Ejercicio 4: Crea un diccionario con la información de un libro ("titulo" y "autor") e imprímelo.
 # Escribe tu código debajo de esta línea:
 
+libros ={
+    "cien años de soledad " : "Gabriel García Márquez",
+    "1984" : "George Orwell",
+    "Don Quijote de la Mancha" : "Miguel de Cervantes"
+}
+print(libros)
