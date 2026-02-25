05b13eb82660ff6d2218f73164459fffc589eb70
sharon <sharonasprilla1724@gmail.com>
2026-02-25 12:34:44 -0500

    primeros ejercicios

--- ejercicios/ejercicio_01.py
+++ ejercicios/ejercicio_01.py
@@ -1,13 +1,19 @@
 # Tema: Python Básico
 
 # Ejercicio 1: Escribe un programa que imprima en pantalla el mensaje "Hola, Mundo!".
+print("hola, mundo!")
 # Escribe tu código debajo de esta línea:
+    # Sharon Saray Asprilla cordoba 
 
 
 # Ejercicio 2: Usa la función print() para mostrar el nombre de tu película o libro favorito.
 # Escribe tu código debajo de esta línea:
+print("mi nombre, kdrama")
+
 
 
 # Ejercicio 3: Imprime tres palabras diferentes en una sola línea utilizando un solo print().
 # Escribe tu código debajo de esta línea:
+print("informatica", "fisica", "ingenieria")
+
 
--- ejercicios/ejercicio_02.py
+++ ejercicios/ejercicio_02.py
@@ -3,14 +3,24 @@
 # Ejercicio 1: Crea una variable llamada 'nombre' y asígnale tu nombre.
 # Luego, crea una variable llamada 'edad' y asígnale tu edad. Imprime ambas.
 # Escribe tu código debajo de esta línea:
+nombre = "sharon"
+edad = 18
+print(nombre, edad)
+
+
 
 
 # Ejercicio 2: Crea una variable llamada 'precio' con el valor 19.99 y una variable 'cantidad' con el valor 3.
 # Imprime ambas variables.
 # Escribe tu código debajo de esta línea:
+precio = 19.99
+cantidad = 3
+print(precio, cantidad)
 
 
 # Ejercicio 3: Asigna el valor de la variable 'a' a una nueva variable 'b'. Imprime 'b'.
 a = "Hola, aprendiendo Python"
+b = a
+print(b)
 # Escribe tu código debajo de esta línea:
 
