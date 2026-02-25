036c53f10732d30baf28d1d3a6256586c4e61077
sharon <sharonasprilla1724@gmail.com>
2026-02-25 13:19:48 -0500

    cambio 4

--- ejercicios/ejercicio_08.py
+++ ejercicios/ejercicio_08.py
@@ -4,6 +4,7 @@
 # cada fruta en mayúsculas.
 frutas = ["manzana", "banana", "cereza", "naranja"]
 # Escribe tu código debajo de esta línea:
+
 for fruta in frutas:
     print(fruta.upper())
 
@@ -13,6 +14,10 @@ for fruta in frutas:
 calificaciones = {"Matemáticas": 9.5, "Historia": 8.0, "Ciencias": 10.0}
 # Escribe tu código debajo de esta línea:
 
+for materia, nota in calificaciones.items():
+    print("En la materia ", materia + ", la calificación es ", nota)
+
+
 
 # Ejercicio 3: Dada una lista de números, usa un ciclo para iterar sobre ellos y 
 # crear una nueva lista llamada 'pares' que contenga solo los números que son divisibles por 2. 
@@ -20,4 +25,11 @@ calificaciones = {"Matemáticas": 9.5, "Historia": 8.0, "Ciencias": 10.0}
 numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 pares = []
 # Escribe tu código debajo de esta línea:
+
+for numero in numeros:
+    if numero % 2 == 0:  
+        pares.append(numero) 
+
+print("La lista de números pares es ", pares)
+
 
