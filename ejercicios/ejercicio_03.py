36e61adf5bc13eed0d908be75f6edf2ac8e4fd12
sharon <sharonasprilla1724@gmail.com>
2026-02-25 12:41:13 -0500

    pruebas de cambio de valor

--- ejercicios/ejercicio_03.py
+++ ejercicios/ejercicio_03.py
@@ -3,6 +3,11 @@
 # Ejercicio 1: Dadas dos variables 'a' y 'b', realiza e imprime la suma, resta, multiplicación y división.
 a = 10
 b = 5
+suma = a + b
+resta = a - b
+multiplicacion = a * b
+division = a / b
+print(suma, resta, multiplicacion, division)
 # Escribe tu código debajo de esta línea:
 
 
@@ -11,7 +16,12 @@ base = 8
 altura = 4
 # Escribe tu código debajo de esta línea:
 
+area = base * altura
+print(area)
+
 
 # Ejercicio 3: Usa el operador módulo (%) para encontrar el residuo de dividir 15 entre 4 e imprímelo.
 # Escribe tu código debajo de esta línea:
+residuo = 15 % 4
+print(residuo)
 
--- ejercicios/ejercicio_04.py
+++ ejercicios/ejercicio_04.py
@@ -2,18 +2,35 @@
 
 # Ejercicio 1: Escribe un programa que verifique si la variable 'numero' es positiva, negativa o cero.
 # Imprime un mensaje indicando el resultado.
-numero = 7
+numero = -9
 # Escribe tu código debajo de esta línea:
+if numero > 0:
+    print("El número es positivo")
+elif numero < 0:
+    print("El número es negativo")
+else:
+    print("El número es cero")
+
+
+
 
 
 # Ejercicio 2: Dada la variable 'edad', escribe una condición que imprima "Mayor de edad" 
 # si es 18 o mayor, y "Menor de edad" en caso contrario.
 edad = 20
 # Escribe tu código debajo de esta línea:
+if edad >= 18:
+    print("Mayor de edad")
+else:
+    print("Menor de edad")
 
 
 # Ejercicio 3: Determina si un número es par o impar usando el operador módulo (%) 
 # y muestra un mensaje acordemente.
 numero_secreto = 14
 # Escribe tu código debajo de esta línea:
+if numero_secreto % 2 == 0:
+    print("El número es par")
+else:
+    print("El número es impar")
 
